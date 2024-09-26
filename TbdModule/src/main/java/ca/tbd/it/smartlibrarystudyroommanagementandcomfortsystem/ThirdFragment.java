5efc0a2d2e21e67eeda3168ad7f832e169b0cdd2
MathewAnderson6706 <hackerxbox2@gmail.com>
2024-09-26 11:06:31 -0400

    *Implementing part of bottom nav
    -Implemented the Java files for the fragments
    -Implemented the fragments into main activity
    -Now have a working bottom nav as a base

---
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/ThirdFragment.java
@@ -0,0 +1,31 @@
+//Mathew Anderson, n01436706
+package ca.tbd.it.smartlibrarystudyroommanagementandcomfortsystem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThirdFragment extends Fragment {
+
+
+
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
+
+
+
+        return view;
+    }
+}

26054293969452cf8a1d95e2276aa775c8277159
MathewAnderson6706 <hackerxbox2@gmail.com>
2024-09-27 12:58:45 -0400

    Implemented a SplashScreen after the default android splash screen.  It contains the image with some text on the top and bottom.

---
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SplashActivity.java
@@ -0,0 +1,32 @@
+package ca.tbd.it.smartlibrarystudyroommanagementandcomfortsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+
+    }
+}

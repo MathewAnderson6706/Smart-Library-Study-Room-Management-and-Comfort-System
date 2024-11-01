9733fe88ad9b3fa867c4a7cb6bbe0fdba43808cb
MediMuamba <medimuamba@icloud.com>
2024-11-01 18:40:36 -0400

    -Added svg vector file and new vector assets. -Added new fragment links in the options menu -Added settings screen -Implementing Night Mode in settings screen. -Adding Vector assets and implementing notifications setting. -New Vector files added. -Font size functionality added in settings screen. -Completed settings screen design and starting Implementations

--- TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
@@ -9,11 +9,27 @@ Section RCB
 package ca.tbd.it.smartlibrarystudyroommanagementandcomfortsystem;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends Fragment {
 
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+
+        return view;
+    }
 }

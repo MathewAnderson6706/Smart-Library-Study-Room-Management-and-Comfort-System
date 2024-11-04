9f46d93e0a1ff37be1d376f826e185042928e523
MediMuamba <medimuamba@icloud.com>
2024-11-04 16:19:04 -0500

    -Added svg vector file and new vector assets. -Added new fragment links in the options menu -Added settings screen -Implementing Night Mode in settings screen. -Adding Vector assets and implementing notifications setting. -New Vector files added. -Font size functionality added in settings screen. -Completed settings screen design and starting Implementations -Implemented dial run time permission in settings screen.

--- TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
@@ -8,28 +8,45 @@ Section RCB
  */
 package ca.tbd.it.smartlibrarystudyroommanagementandcomfortsystem;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends Fragment {
 
+
     public SettingsFragment() {
         // Required empty public constructor
     }
 
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-
-
+        
+        ImageButton tbd_call = view.findViewById(R.id.tbd_call);
+        tbd_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1234567789"));
+                startActivity(intent);
+            }
+        });
+        
         return view;
+        
     }
+    
+    
 }

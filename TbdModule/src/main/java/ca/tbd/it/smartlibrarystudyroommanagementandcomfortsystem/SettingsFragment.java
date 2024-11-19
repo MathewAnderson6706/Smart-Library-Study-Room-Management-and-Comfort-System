3356dfc92dc975a7fd7be6921a5d2df751d64ccb
MediMuamba <medimuamba@icloud.com>
2024-11-19 14:05:30 -0500

    -Added svg vector file and new vector assets. -Added new fragment links in the options menu -Added settings screen -Implementing Night Mode in settings screen. -Adding Vector assets and implementing notifications setting. -New Vector files added. -Font size functionality added in settings screen. -Completed settings screen design and starting Implementations -Implemented dial run time permission in settings screen. -Feedback in settings now takes us to feedback screen. -Back button implementation -Updated background colors in all screens for better screen flow. -Location runtime permission implemented. -Room settings implementation continued. -Modifying menu items layout. -Removed call option from settings screen. -Complications with menu item background. -Moving room settings implementations to one screen. -Updated the room settings screen so Lighting, Temperature and Time are tracked in one screen. -Room settings design

--- TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/SettingsFragment.java
@@ -53,17 +53,7 @@ public class SettingsFragment extends Fragment {
             }
         });
 
-
-        ImageButton tbdcall = view.findViewById(R.id.tbd_call);
-        tbdcall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse(getString(R.string.tel_1234567789)));
-                startActivity(intent);
-            }
-        });
-
+        
         ImageView exit = view.findViewById(R.id.settings_exit);
         exit.setOnClickListener(new View.OnClickListener() {
             @Override

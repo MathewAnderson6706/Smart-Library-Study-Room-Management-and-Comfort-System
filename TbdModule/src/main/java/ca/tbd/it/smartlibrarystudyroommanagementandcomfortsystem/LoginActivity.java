0bc5ce6cd1dae4dc411503cf02d9dbec663e3180
MathewAnderson6706 <hackerxbox2@gmail.com>
2024-11-25 17:05:27 -0500

    Working on google sign in -Currently having issues with the imports

--- TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/LoginActivity.java
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/LoginActivity.java
@@ -19,6 +19,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {

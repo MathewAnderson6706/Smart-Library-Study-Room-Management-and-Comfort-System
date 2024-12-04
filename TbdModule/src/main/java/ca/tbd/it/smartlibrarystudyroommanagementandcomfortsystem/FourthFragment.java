fb16585e2ea64b1c60799034e70cc425119cf2d1
MediMuamba <medimuamba@icloud.com>
2024-12-03 22:14:33 -0500

    -Added a vector asset to include for the FAB. -The FAB will display in the room fragments and prompt the user to generate a code in order to access the room. -When clicked, a code is generated to enter the room. -Fixed the floating action bar to simply display a message that this screen requires a code to enter the rooms. -Added FAB to SecondFragment to maintain it throughout all room screens. -Added FAB to ThirdFragment -Lastly added FAB to FourthFragment -In second fragment xml, added the fab button to match the home fragment. -Continued the FAB into the third and fourth fragments -Offline mode implemented for Home fragment. This will show a toast when checking network connectivity, and room details will be available offline. -Removed unsused import statements

--- TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/FourthFragment.java
+++ TbdModule/src/main/java/ca/tbd/it/smartlibrarystudyroommanagementandcomfortsystem/FourthFragment.java
@@ -10,8 +10,7 @@ package ca.tbd.it.smartlibrarystudyroommanagementandcomfortsystem;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.core.content.ContextCompat;
+
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
